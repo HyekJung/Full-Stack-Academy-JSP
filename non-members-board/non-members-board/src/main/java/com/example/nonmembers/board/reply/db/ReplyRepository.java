c6f1575d26842aec60b5cc45b1c4d783edbc350d
HyekJung <155180714+HyekJung@users.noreply.github.com>
2024-03-21 17:18:32 +0900

    Delete non-members-board/non-members-board directory

--- non-members-board/non-members-board/src/main/java/com/example/nonmembers/board/reply/db/ReplyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.nonmembers.board.reply.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<ReplyEntity, Long> {
-}
