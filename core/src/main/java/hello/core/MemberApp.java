d15695247fd0abcb52f67c052c5badaa2664b78f
HyekJung <155180714+HyekJung@users.noreply.github.com>
2024-04-04 21:34:28 +0900

    [Java] 스프링 컨테이너 등록

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -5,12 +5,18 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-//        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); //스프링 컨테이너에 bean등록된걸 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //이름과 타입
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -3,23 +3,29 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //해당 클래스에는 하나 이상의 @Bean 메서드가 정의되어 있음을 의미
 public class AppConfig { //애플리케이션의 실제 동작에 필요한 "구현 객체 생성", 생성자 주입(의존성)
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
     }
-
-    private static MemoryMemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceimpl(MemberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
